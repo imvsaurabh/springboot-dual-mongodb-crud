3e6070bae9627b74de56f4b809cac32b7ed88570
VishwakarmaSaurabh <321saurabhv@gmail.com>
2022-12-21 20:31:26 +0530

    Initial commit

---
+++ src/main/java/com/saurabh/dualmongodb/DualMongodbCrudApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.dualmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualMongodbCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualMongodbCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/dualmongodb/DualMongodbCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.dualmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualMongodbCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
